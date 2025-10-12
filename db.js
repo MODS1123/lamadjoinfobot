a3f11b1119942ff20fe8350af446de92c190069e
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-13 04:51:40 +0500

    Update database - 2025-10-13T01:51:40.549900

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  "codes": {
+    "JTU1": {
+      "discord_id": 852959671353606155,
+      "discord_username": "lmj_mods",
+      "discord_user": "lmj_mods",
+      "discord_server_nick": "Alberto",
+      "created_at": "2025 12 10 23:51",
+      "expires_at": "2025 12 10 23:53",
+      "used": false
+    }
+  }
+};
