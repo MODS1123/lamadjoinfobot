bd01f2412be1265d2b86e7ce518719f607f246a8
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-13 03:19:37 +0500

    Update database - 2025-10-13T03:19:37.260100

--- db.js
+++ db.js
@@ -13,20 +13,20 @@ module.exports = {
         "telegram_username": "LMJSHOP",
         "activated_at": "2025-10-13T03:17:53.059016"
       },
-      "telegram_subscribed": false,
+      "telegram_subscribed": true,
       "telegram_resubscribed_at": "2025-10-13T03:18:04.762891",
       "telegram_unsubscribed_at": "2025-10-13T03:18:46.000934",
       "telegram_banned": false,
       "telegram_banned_at": "2025-10-13T03:18:46.214232",
       "telegram_unsubscribed": false,
-      "telegram_requires_subscription": true,
       "telegram_unbanned_at": "2025-10-13T03:19:23.281598",
       "telegram_unbanned_by": {
         "moderator_id": 921182112,
         "moderator_username": "Alberto114"
       },
       "grace_period_until": "2025-10-13T15:19:23.282478",
-      "telegram_unban_previous_reason": "unsubscribed"
+      "telegram_unban_previous_reason": "unsubscribed",
+      "telegram_subscribed_at": "2025-10-13T03:19:36.701377"
     }
   }
 };
