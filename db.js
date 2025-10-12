9e942d77e5499431baf5f132016046123aa9bb87
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-13 03:16:43 +0500

    Update database - 2025-10-13T03:16:43.382938

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  "codes": {
+    "QEVU": {
+      "discord_id": 852959671353606155,
+      "discord_username": "lmj_mods",
+      "discord_user": "lmj_mods",
+      "discord_server_nick": "Alberto_Lmj",
+      "created_at": "2025 12 10 22:16",
+      "expires_at": "2025 12 10 22:18",
+      "used": false
+    }
+  }
+};
