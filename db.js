a44d663b6c24f25905d8f0b6e4ed0a32ad6a45dc
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-14 03:31:23 +0500

    Update database - 2025-10-14T00:31:23.781960

--- db.js
+++ db.js
@@ -7,7 +7,12 @@ module.exports = {
       "discord_server_nick": "Lex",
       "created_at": "2025 13 10 22:30",
       "expires_at": "2025 13 10 22:32",
-      "used": false
+      "used": true,
+      "activated_by": {
+        "telegram_id": 528167960,
+        "telegram_username": "Lex_M_A_V",
+        "activated_at": "2025-10-14T00:31:23.574998"
+      }
     }
   }
 };
