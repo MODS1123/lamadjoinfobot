a49f4d55335b256c44f054b70c884ed0cf9a015c
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-14 03:30:32 +0500

    Update database - 2025-10-14T00:30:32.006711

--- db.js
+++ db.js
@@ -1,3 +1,13 @@
 module.exports = {
-  "codes": {}
+  "codes": {
+    "CI79": {
+      "discord_id": 430448539806597150,
+      "discord_username": "lexmav",
+      "discord_user": "lexmav",
+      "discord_server_nick": "Lex",
+      "created_at": "2025 13 10 22:30",
+      "expires_at": "2025 13 10 22:32",
+      "used": false
+    }
+  }
 };
