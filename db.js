24abcd1c50cccb0c6913c64d27e8e76b4fb644c2
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-18 06:09:19 +0500

    Update database - 2025-10-18T03:09:18.725466

--- db.js
+++ db.js
@@ -287,11 +287,11 @@ module.exports = {
         "telegram_username": "Djoni_1",
         "activated_at": "2025-10-18T02:58:34.202145"
       },
-      "telegram_subscribed": true,
+      "telegram_subscribed": false,
       "telegram_resubscribed_at": "2025-10-18T03:07:53.546407",
-      "telegram_unsubscribed_at": "2025-10-18T03:03:32.910166",
-      "telegram_banned": false,
-      "telegram_banned_at": "2025-10-18T03:03:32.977449",
+      "telegram_unsubscribed_at": "2025-10-18T03:09:18.348827",
+      "telegram_banned": true,
+      "telegram_banned_at": "2025-10-18T03:09:18.524816",
       "telegram_unsubscribed": false,
       "telegram_requires_subscription": true,
       "telegram_unbanned_at": "2025-10-18T03:06:53.460905",
@@ -301,7 +301,8 @@ module.exports = {
       },
       "telegram_unban_previous_reason": "unsubscribed",
       "last_invite_issued_at": "2025-10-18T03:08:10.694281",
-      "invite_link_issued": true
+      "invite_link_issued": true,
+      "telegram_ban_reason": "unsubscribed"
     }
   }
 };
