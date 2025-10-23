bc89cd3ce069e65829e6c03c84d3818f9ba11094
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-23 22:20:12 +0500

    Update database - 2025-10-23T19:20:12.293070

--- db.js
+++ db.js
@@ -246,7 +246,7 @@ module.exports = {
       "telegram_subscribed": false,
       "telegram_resubscribed_at": "2025-10-14T17:51:07.454112",
       "discord_removed": true,
-      "discord_removed_at": "2025-10-23T17:20:10.808801Z",
+      "discord_removed_at": "2025-10-23T17:15:07.837277Z",
       "discord_removed_display_name": null,
       "telegram_banned": true,
       "telegram_banned_at": "2025-10-23T17:15:09.281058Z",
@@ -544,7 +544,9 @@ module.exports = {
         "telegram_id": 916243344,
         "telegram_username": "Pina_Colaadaa",
         "activated_at": "2025-10-23T19:20:07.347682"
-      }
+      },
+      "telegram_subscribed": true,
+      "telegram_resubscribed_at": "2025-10-23T19:20:12.021716"
     }
   }
 };
