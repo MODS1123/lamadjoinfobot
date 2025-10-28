f12504bfec8723bca1efb1d590c1743ec1cf2f22
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-28 14:55:47 +0500

    Update database - 2025-10-28T10:55:47.212409

--- db.js
+++ db.js
@@ -456,9 +456,17 @@ module.exports = {
       "telegram_subscribed": false,
       "telegram_resubscribed_at": "2025-10-23T18:10:25.588852",
       "telegram_unsubscribed_at": "2025-10-23T19:25:02.305032",
-      "telegram_banned": true,
+      "telegram_banned": false,
       "telegram_banned_at": "2025-10-23T19:25:02.450457",
-      "telegram_ban_reason": "unsubscribed"
+      "telegram_unsubscribed": false,
+      "telegram_requires_subscription": true,
+      "telegram_unbanned_at": "2025-10-28T10:55:46.848150",
+      "telegram_unbanned_by": {
+        "moderator_id": 528167960,
+        "moderator_username": "Lex_M_A_V"
+      },
+      "grace_period_until": "2025-10-28T22:55:46.848161",
+      "telegram_unban_previous_reason": "unsubscribed"
     },
     "7DXF": {
       "discord_id": 1375363925096398910,
