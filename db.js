7abb23b3c1373ef051695d8d0256edde1ef7be18
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-02 03:29:06 +0500

    Update database - 2025-11-01T23:29:04.318273

--- db.js
+++ db.js
@@ -1480,7 +1480,7 @@ module.exports = {
       "telegram_resubscribed_at": "2025-10-27T22:53:35.180306",
       "telegram_unsubscribed_at": "2025-10-28T19:48:13.463318",
       "telegram_banned": true,
-      "telegram_banned_at": "2025-11-01T22:29:00.945062Z",
+      "telegram_banned_at": "2025-11-01T22:23:14.874572Z",
       "telegram_ban_reason": "not_in_guild_check",
       "discord_removed": true,
       "discord_removed_at": "2025-11-01T22:28:54.504286Z"
@@ -1501,10 +1501,10 @@ module.exports = {
       "telegram_subscribed": false,
       "telegram_resubscribed_at": "2025-10-27T23:05:36.771716",
       "discord_removed": true,
-      "discord_removed_at": "2025-11-01T22:29:02.794162Z",
+      "discord_removed_at": "2025-11-01T22:23:16.397624Z",
       "discord_removed_display_name": null,
       "telegram_banned": true,
-      "telegram_banned_at": "2025-11-01T22:29:04.454225Z",
+      "telegram_banned_at": "2025-11-01T22:23:17.774672Z",
       "telegram_ban_reason": "not_in_guild_check",
       "telegram_unsubscribed_at": "2025-10-28T07:41:51.855618"
     },
@@ -1599,8 +1599,12 @@ module.exports = {
         "telegram_username": "MOHCTP_CBO",
         "activated_at": "2025-10-28T23:10:59.905220"
       },
-      "telegram_subscribed": true,
-      "telegram_resubscribed_at": "2025-10-28T23:11:04.475159"
+      "telegram_subscribed": false,
+      "telegram_resubscribed_at": "2025-10-28T23:11:04.475159",
+      "telegram_unsubscribed_at": "2025-11-01T23:29:03.722995",
+      "telegram_banned": true,
+      "telegram_banned_at": "2025-11-01T23:29:03.755659",
+      "telegram_ban_reason": "unsubscribed"
     },
     "2FTB": {
       "discord_id": 824327858930843729,
