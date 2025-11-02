80fbc5349b1d0fcaca47ae2609399150eb2fa075
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-02 23:27:29 +0500

    Update database - 2025-11-02T19:27:29.010759

--- db.js
+++ db.js
@@ -2279,6 +2279,15 @@ module.exports = {
       },
       "telegram_subscribed": true,
       "telegram_resubscribed_at": "2025-11-02T18:59:26.135889"
+    },
+    "X920": {
+      "discord_id": 1399747125591408694,
+      "discord_username": "leoleon001",
+      "discord_user": "leoleon001",
+      "discord_server_nick": "Tujeo",
+      "created_at": "2025 02 11 18:27",
+      "expires_at": "2025 02 11 18:29",
+      "used": false
     }
   }
 };
