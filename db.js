88fde712ca5f17719d8c575b494aef5377de1db1
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-04 21:41:24 +0500

    Update database - 2025-11-04T17:41:24.177738

--- db.js
+++ db.js
@@ -2090,7 +2090,7 @@ module.exports = {
       "telegram_banned_at": "2025-11-04T16:35:08.482371Z",
       "telegram_ban_reason": "not_in_guild_check",
       "discord_removed": true,
-      "discord_removed_at": "2025-11-04T16:35:06.796718Z"
+      "discord_removed_at": "2025-11-04T16:41:23.241667Z"
     },
     "J9TZ": {
       "discord_id": 710976667258191942,
