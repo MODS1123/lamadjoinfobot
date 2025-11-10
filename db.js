cb5075bd9b3d296abd0581557e0757c4082a0a6d
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-10 23:59:10 +0500

    Update database - 2025-11-10T19:59:09.715273

--- db.js
+++ db.js
@@ -2663,6 +2663,15 @@ module.exports = {
       },
       "telegram_subscribed": true,
       "telegram_resubscribed_at": "2025-11-10T19:28:04.872147"
+    },
+    "BEMK": {
+      "discord_id": 945604048789307392,
+      "discord_username": "ggff2580",
+      "discord_user": "ggff2580",
+      "discord_server_nick": "AlenI",
+      "created_at": "2025 10 11 18:59",
+      "expires_at": "2025 10 11 19:01",
+      "used": false
     }
   }
 };
