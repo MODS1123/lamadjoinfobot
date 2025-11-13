0656c57b446dac71f1a970475116264a425ed395
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-13 17:34:38 +0500

    Update database - 2025-11-13T13:34:37.765380

--- db.js
+++ db.js
@@ -2757,15 +2757,6 @@ module.exports = {
       "telegram_subscribed": true,
       "telegram_resubscribed_at": "2025-11-13T11:45:33.056113"
     },
-    "3677": {
-      "discord_id": 1099367474513850379,
-      "discord_username": "stones1488",
-      "discord_user": "stones1488",
-      "discord_server_nick": "Mbappe",
-      "created_at": "2025 13 11 12:32",
-      "expires_at": "2025 13 11 12:34",
-      "used": false
-    },
     "F4P0": {
       "discord_id": 1099367474513850379,
       "discord_username": "stones1488",
