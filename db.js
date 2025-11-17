46f7bffddc73e96e85d8717ed01ee07ca42b53bc
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-11-18 00:58:58 +0500

    Update database - 2025-11-17T20:58:57.889822

--- db.js
+++ db.js
@@ -592,7 +592,7 @@ module.exports = {
       "last_invite_issued_at": "2025-10-23T18:31:45.815182",
       "invite_link_issued": true,
       "discord_removed": true,
-      "discord_removed_at": "2025-11-17T19:58:56.267748Z",
+      "discord_removed_at": "2025-11-17T19:50:45.964020Z",
       "discord_removed_display_name": null,
       "telegram_unsubscribed_at": "2025-10-28T11:49:27.487888",
       "telegram_banned": true,
@@ -2947,6 +2947,15 @@ module.exports = {
       },
       "telegram_subscribed": true,
       "telegram_resubscribed_at": "2025-11-17T20:18:21.990441"
+    },
+    "A8SB": {
+      "discord_id": 1197117212385878087,
+      "discord_username": "albsefgpspbvgbvzv_19777",
+      "discord_user": "albsefgpspbvgbvzv_19777",
+      "discord_server_nick": "AlbertoV",
+      "created_at": "2025 17 11 19:58",
+      "expires_at": "2025 17 11 20:03",
+      "used": false
     }
   }
 };
